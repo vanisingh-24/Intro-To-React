b30b743ce9ee571635a4916f8a0520c567f6361a
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2021-04-10 16:19:54 +0530

    Components added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
 import './App.css';
 
 const App = () => (
+  <Router>
     <Fragment>
-      <h1>App</h1>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
     </Fragment>
+  </Router>
 );
 
 export default App;
